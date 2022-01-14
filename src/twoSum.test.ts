fe67ff245ab16f608af6f68b06dbefca23c8cb99
Behzad AM <behzad.am@gmail.com>
2022-01-14 13:05:22 +0330

    test: update tests

--- src/twoSum.test.ts
+++ src/twoSum.test.ts
@@ -1,5 +1,27 @@
 import twoSum from "./twoSum"
 
-it('should return the result', () => {
-  expect(twoSum([2,7,11,15], 9)).toStrictEqual([0,1])
-})
+const validCases = test.each`
+  nums                |    target  |     expected
+  ${[2, 7, 11, 15]}   |    ${9}    |     ${[0, 1]}
+  ${[2, 7, 11, 15]}   |    ${18}   |     ${[1, 2]}
+  ${[2, 7, 11, 15]}   |    ${26}   |     ${[2, 3]}
+`
+
+const invalidCases = test.each`
+  nums                |    target  |     expected
+  ${[2, 7, 11, 15]}   |    ${100}  |     ${[]}
+  ${[2, 7, 11, 15]}   |    ${102}  |     ${[]}
+  ${[2, 7, 11, 15]}   |    ${70}   |     ${[]}
+`
+
+describe('twoSum valid cases', () => {
+  validCases('should return $expected when input is: $nums and target is: $target', ({ nums, target, expected }) => {
+    expect(twoSum(nums, target)).toStrictEqual(expected)
+  })
+});
+
+describe('twoSum invalid cases', () => {
+  invalidCases('should return $expected when input is: $nums and target is: $target', ({ nums, target, expected }) => {
+    expect(twoSum(nums, target)).toStrictEqual(expected)
+  })
+});
