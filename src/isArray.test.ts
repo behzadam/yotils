77aae5932f6e17dac0367b3225cbf96d8954b0a4
Behzad AM <behzad.am@gmail.com>
2022-01-25 18:14:44 +0330

    test: add test

---
+++ src/isArray.test.ts
@@ -0,0 +1,32 @@
+import isArray from "./isArray";
+import { describeGen } from "./utils/describeGen";
+
+const validCases = test.each`
+  input             | expected
+  ${[1, 1, 1]}      | ${true}
+  ${["one", "one"]} | ${true}
+`;
+
+const invalidCases = test.each`
+  input       | expected
+  ${"string"} | ${false}
+  ${null}     | ${false}
+`;
+
+describe(describeGen(isArray.name).valid, () => {
+  validCases(
+    "should return $expected when input is: $input",
+    ({ input, expected }) => {
+      expect(isArray(input)).toBe(expected);
+    }
+  );
+});
+
+describe(describeGen(isArray.name).valid, () => {
+  invalidCases(
+    "should return $expected when input is: $input",
+    ({ input, expected }) => {
+      expect(isArray(input)).toBe(expected);
+    }
+  );
+});
