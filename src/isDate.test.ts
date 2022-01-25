42da6472b9ee3f8f37b97131129dec155a1508fb
Behzad AM <behzad.am@gmail.com>
2022-01-25 23:44:43 +0330

    test: add test

---
+++ src/isDate.test.ts
@@ -0,0 +1,9 @@
+import isDate from "./isDate";
+import { describeGen } from "./utils/describeGen";
+
+describe(describeGen(isDate.name).valid, () => {
+  test("should return true on date type", () => {
+    expect(isDate(new Date())).toBe(true);
+    expect(isDate("2021-10-10")).toBe(false);
+  });
+});
