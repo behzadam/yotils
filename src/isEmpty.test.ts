6f4972251da60a258764696f30c0255f3a50f421
Behzad AM <behzad.am@gmail.com>
2022-01-26 00:18:25 +0330

    test: add test

---
+++ src/isEmpty.test.ts
@@ -0,0 +1,18 @@
+import isEmpty from "./isEmpty";
+import { describeGen } from "./utils/describeGen";
+
+describe(describeGen(isEmpty.name).valid, () => {
+  test("should return true on empty values", () => {
+    expect(isEmpty({})).toBe(true);
+    expect(isEmpty([])).toBe(true);
+    expect(isEmpty("")).toBe(true);
+  });
+});
+
+describe(describeGen(isEmpty.name).invalid, () => {
+  test("should return false on non empty values", () => {
+    expect(isEmpty({ id: 1 })).toBe(false);
+    expect(isEmpty([1, 2])).toBe(false);
+    expect(isEmpty("hello")).toBe(false);
+  });
+});
