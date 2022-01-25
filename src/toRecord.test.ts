93fa61d83a9f6ea4682eb8ef70a3c0cce512dcbc
Behzad AM <behzad.am@gmail.com>
2022-01-25 21:09:29 +0330

    test: add test

---
+++ src/toRecord.test.ts
@@ -0,0 +1,18 @@
+import toRecord from "./toRecord";
+import { describeGen } from "./utils/describeGen";
+
+describe(describeGen(toRecord.name).valid, () => {
+  test("should return record of an array", () => {
+    const array = [
+      { id: 1, name: "A" },
+      { id: 2, name: "B" },
+      { id: 3, name: "C" },
+    ];
+    const record = toRecord(array, "id");
+    expect(record).toStrictEqual({
+      "1": { id: 1, name: "A" },
+      "2": { id: 2, name: "B" },
+      "3": { id: 3, name: "C" },
+    });
+  });
+});
