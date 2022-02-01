84b7e4e610f452b562c6e4a70cf0aea40fa6f736
Behzad AM <behzad.am@gmail.com>
2022-02-01 19:06:46 +0330

    test: add test

---
+++ src/wrapInArray.test.ts
@@ -0,0 +1,17 @@
+import { describeGen } from "./utils/describeGen";
+import wrapInArray from "./wrapInArray";
+
+const validCases = test.each`
+  target | expected
+  ${"A"} | ${["A"]}
+  ${1}   | ${[1]}
+  ${[1]} | ${[[1]]}
+`;
+describe(describeGen(wrapInArray.name).valid, () => {
+  validCases(
+    "should return $expected when target is $target",
+    ({ target, expected }) => {
+      expect(wrapInArray(target)).toStrictEqual(expected);
+    }
+  );
+});
