0f1c67a74cf8177c0f8a86777348dc0acd58b389
Behzad AM <behzad.am@gmail.com>
2022-02-03 11:39:08 +0330

    test: add test

---
+++ src/removeEmptyKeys.test.ts
@@ -0,0 +1,11 @@
+import removeEmptyKeys from "./removeEmptyKeys";
+
+test("removeEmptyKeys", () => {
+  const result = removeEmptyKeys({
+    key1: "1",
+    key2: "2",
+    key3: "",
+    key4: { key5: "" },
+  });
+  console.log(result);
+});
