348181c5a92c59b6e9a283c4d6936d4ab58f1dda
Behzad AM <behzad.am@gmail.com>
2022-02-03 11:39:02 +0330

    feat: add removeEmptyKeys

---
+++ src/removeEmptyKeys.ts
@@ -0,0 +1,12 @@
+function removeEmptyKeys(input: Record<any, any>): Record<any, any> {
+  return Object.keys(input)
+    .filter((key) => input[key] !== "")
+    .reduce((result, key) => {
+      return {
+        ...result,
+        [key]: input[key],
+      };
+    }, {});
+}
+
+export default removeEmptyKeys;
