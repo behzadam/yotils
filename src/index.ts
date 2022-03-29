8996776edd14e58ac0cfb393463578af709d0a05
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-03-29 23:32:10 +0430

    feat: add hasDuplicates to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // array
 import areSame from "./array/areSame";
 import countItems from "./array/countItems";
+import hasDuplicates from "./array/hasDuplicates";
 
 import isEqual from "./util/isEqual";
 
-export { areSame, countItems, isEqual };
+export { areSame, countItems, hasDuplicates, isEqual };
