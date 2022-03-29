cab15f87cf5a6aeb7c821b26ede323003c32dfe4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-03-29 23:43:17 +0430

    feat: add wrapInArray

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@
 import areSame from "./array/areSame";
 import countItems from "./array/countItems";
 import hasDuplicates from "./array/hasDuplicates";
+import isArray from "./array/isArray";
+import wrapInArray from "./array/wrapInArray";
 
 import isEqual from "./util/isEqual";
 
-export { areSame, countItems, hasDuplicates, isEqual };
+export { areSame, countItems, hasDuplicates, wrapInArray, isArray, isEqual };
