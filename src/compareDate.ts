2e4f0c81b5f63600790e94af8c04d34cc9c28e54
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-24 23:29:08 +0330

    docs: add comments

--- src/compareDate.ts
+++ src/compareDate.ts
@@ -1,5 +1,13 @@
-function compareDate(target: Required<Date>, other: Required<Date>): boolean { 
-  return target.getTime() === other.getTime()
+/**
+ * Compares two given date type values.
+ * @param target - Date
+ * @param other - Date
+ * @returns true if target.getTime() equals other.getTime()
+ * @remarks Date.prototype.getTime() returns number of milliseconds.
+ * @beta
+ */
+function compareDate(target: Required<Date>, other: Required<Date>): boolean {
+  return target.getTime() === other.getTime();
 }
 
 export default compareDate;
