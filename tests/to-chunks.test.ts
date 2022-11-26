61337ded5fb98db02c9fe298a0a862dd81763ac4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-26 16:52:22 +0330

    refactor: rename to kebab-case

--- tests/to-chunks.test.ts
+++ tests/to-chunks.test.ts
@@ -3,6 +3,7 @@ import toChunks from "../src/to-chunks";
 describe("splitIntoChunks", () => {
   test("should return array of chunks or an empty array", () => {
     let chunks = toChunks("Lorem ipsum dolor sit amet, consectetur adipis", 4);
+    console.log(chunks);
     expect(chunks.length).toBe(4);
     chunks = toChunks("", 4);
     expect(chunks.length).toBe(0);
