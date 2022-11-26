ed77839994c5927e190f3efa5dc178393f7a00f7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-26 17:21:11 +0330

    chore: change jest config to a json file

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
