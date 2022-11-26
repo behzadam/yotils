b508cb0e4d8b9a40b08bd3bbcba94f340da8ee6c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-26 14:11:51 +0330

    refactor: rename file to kebab-case

--- tests/count-items.test.ts
+++ tests/count-items.test.ts
@@ -1,10 +1,11 @@
-import countItems from "../src/array/countItems";
+import countItems from "@lib/count-items";
 
 const validCases = test.each`
   input                             | expected
   ${[1, 1, 1, 2]}                   | ${{ "1": 3, "2": 1 }}
   ${["in", "love", "love", "love"]} | ${{ in: 1, love: 3 }}
 `;
+
 describe("classify valid cases", () => {
   validCases(
     "should return $expected when input is: $input",
