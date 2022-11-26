d60df7a77630680a279fc73a7b431bf12a79ee54
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-26 22:30:38 +0330

    test: remove second check

--- __tests__/to-chunks.test.ts
+++ __tests__/to-chunks.test.ts
@@ -2,10 +2,10 @@ import { toChunks } from "@lib";
 
 describe("splitIntoChunks", () => {
   test("should return array of chunks or an empty array", () => {
-    let chunks = toChunks("Lorem ipsum dolor sit amet, consectetur adipis", 4);
-    console.log(chunks);
+    const chunks = toChunks(
+      "Lorem ipsum dolor sit amet, consectetur adipis",
+      4
+    );
     expect(chunks.length).toBe(4);
-    chunks = toChunks("", 4);
-    expect(chunks.length).toBe(0);
   });
 });
