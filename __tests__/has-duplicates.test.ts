27168a86959e9b30e3431048b22d48077b8d50a5
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 20:14:19 +0330

    tests: merge test cases

--- __tests__/has-duplicates.test.ts
+++ __tests__/has-duplicates.test.ts
@@ -1,31 +1,13 @@
 import { hasDuplicates } from "@lib";
 
-const validCases = test.each`
-  array                    | expected
-  ${[1, 1, 2]}             | ${true}
-  ${["one", "one", "two"]} | ${true}
-`;
-
-const invalidCases = test.each`
-  array                      | expected
-  ${[1, 2, 3]}               | ${false}
-  ${["one", "two", "three"]} | ${false}
-`;
-
-describe("hasDuplicates valid cases", () => {
-  validCases(
-    "should return $expected when array is: $array",
-    ({ array, expected }) => {
-      expect(hasDuplicates(array)).toBe(expected);
-    }
-  );
-});
-
-describe("hasDuplicates invalid cases", () => {
-  invalidCases(
-    "should return $expected when array is: $array",
-    ({ array, expected }) => {
-      expect(hasDuplicates(array)).toBe(expected);
-    }
-  );
+describe("hasDuplicates cases", () => {
+  test.each`
+    array                      | expected
+    ${[1, 1, 2]}               | ${true}
+    ${["one", "one", "two"]}   | ${true}
+    ${[1, 2, 3]}               | ${false}
+    ${["one", "two", "three"]} | ${false}
+  `("should return $expected when array is: $array", ({ array, expected }) => {
+    expect(hasDuplicates(array)).toBe(expected);
+  });
 });
