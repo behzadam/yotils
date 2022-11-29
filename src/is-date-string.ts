905fce30d0740e9a5856d352ca3fef8d9c389a5b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 00:27:26 +0330

    feat: update with regexp

--- src/is-date-string.ts
+++ src/is-date-string.ts
@@ -1,3 +1,21 @@
+/**
+ * Determines whether the input string is valid date or not.
+ * @param value - input string.
+ * @returns true if the input string is valid date, false otherwise.
+ * @example
+ * ```ts
+ * isDateString('01/01/2000') // true
+ * isDateString('31/09/2000') // false
+ * ```
+ * @remarks
+ * While there are some regular expressions that allow date validations,
+ * it is better to validate dates using special date and time libraries like momentjs.
+ * RegEx is from: https://regex101.com/r/IuYz9V/1
+ *
+ * @public
+ */
 export function isDateString(value: string): value is string {
-  return !Number.isNaN(new Date(value).valueOf());
+  const regexp =
+    /(^(?:(?:(?:31(?:(?:([-.\/])(?:0?[13578]|1[02])\2)|(?:([-.\/ ])(?:Jan|JAN|Mar|MAR|May|MAY|Jul|JUL|Aug|AUG|Oct|OCT|Dec|DEC)\3)))|(?:(?:29|30)(?:(?:([-.\/])(?:0?[13-9]|1[0-2])\4)|(?:([-.\/ ])(?:Jan|JAN|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)\5))))(?:(?:1[6-9]|[2-9]\d)?\d{2}))$|^(?:29(?:(?:([-.\/])(?:0?2)\6)|(?:([-.\/ ])(?:Feb|FEB)\7))(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))$|^(?:(?:0?[1-9]|1\d|2[0-8])(?:(?:([-.\/])(?:(?:0?[1-9]|(?:1[0-2])))\8)|(?:([-.\/ ])(?:Jan|JAN|Feb|FEB|Mar|MAR|May|MAY|Jul|JUL|Aug|AUG|Oct|OCT|Dec|DEC)\9))(?:(?:1[6-9]|[2-9]\d)?\d{2}))$)/gm;
+  return regexp.test(value);
 }
