78bdd7240cd602b015efcd10fc586fab8809779e
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 17:11:59 +0330

    docs: add @public tag

--- src/are-same.ts
+++ src/are-same.ts
@@ -1,16 +1,15 @@
 /**
  * Compares all items in the given array and returns true if they are same.
  * @param arr - Given array.
- * @returns - true | false
- * @beta
- *
- * #### Usage
+ * @returns true | false
+ * @example
  *
  * ```ts
  * areSame([1, 1, 1, 1]) // true
  * areSame(["one", "one", "one"]) // true
  * areSame([1, 2]) // false
  * ```
+ * @public
  */
 export function areSame<T extends string | number | boolean>(
   arr: Required<T[]>
