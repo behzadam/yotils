33f7b357591f46232e19ae26f9ea97030c571c3f
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 17:25:57 +0330

    test: move test.each to describe block

--- __tests__/count-items.test.ts
+++ __tests__/count-items.test.ts
@@ -1,16 +1,11 @@
 import { countItems } from "@lib";
 
-const validCases = test.each`
-  input                             | expected
-  ${[1, 1, 1, 2]}                   | ${{ "1": 3, "2": 1 }}
-  ${["in", "love", "love", "love"]} | ${{ in: 1, love: 3 }}
-`;
-
-describe("classify valid cases", () => {
-  validCases(
-    "should return $expected when input is: $input",
-    ({ input, expected }) => {
-      expect(countItems(input)).toStrictEqual(expected);
-    }
-  );
+describe("countItems cases", () => {
+  test.each`
+    input                             | expected
+    ${[1, 1, 1, 2]}                   | ${{ "1": 3, "2": 1 }}
+    ${["in", "love", "love", "love"]} | ${{ in: 1, love: 3 }}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(countItems(input)).toStrictEqual(expected);
+  });
 });
