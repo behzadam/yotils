f9b245e5309059f19d8b57e63a37f14ce9fff7da
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 21:41:42 +0330

    test: merge test cases

--- __tests__/is-array.test.ts
+++ __tests__/is-array.test.ts
@@ -1,32 +1,14 @@
 import { isArray } from "@lib";
-import { describeGen } from "./utils/describe-gen";
 
-const validCases = test.each`
-  input             | expected
-  ${[1, 1, 1]}      | ${true}
-  ${["one", "one"]} | ${true}
-`;
-
-const invalidCases = test.each`
-  input       | expected
-  ${"string"} | ${false}
-  ${null}     | ${false}
-`;
-
-describe(describeGen(isArray.name).valid, () => {
-  validCases(
-    "should return $expected when input is: $input",
-    ({ input, expected }) => {
-      expect(isArray(input)).toBe(expected);
-    }
-  );
-});
-
-describe(describeGen(isArray.name).valid, () => {
-  invalidCases(
-    "should return $expected when input is: $input",
-    ({ input, expected }) => {
-      expect(isArray(input)).toBe(expected);
-    }
-  );
+describe("isArray cases", () => {
+  test.each`
+    input                     | expected
+    ${[1, 1, 1]}              | ${true}
+    ${["one", "one"]}         | ${true}
+    ${[{ id: 1 }, { id: 2 }]} | ${true}
+    ${"string"}               | ${false}
+    ${null}                   | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isArray(input)).toBe(expected);
+  });
 });
