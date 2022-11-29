a412ecd27e32685fe3d949d51d1f224198543d75
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 20:13:56 +0330

    docs: add @example

--- src/has-duplicates.ts
+++ src/has-duplicates.ts
@@ -4,6 +4,11 @@ import { Primitive } from "./types";
  * Determines whether an array has duplicates.
  * @param array - input
  * @returns true if array has duplicates, false otherwise.
+ * @example
+ * ```ts
+ * hasDuplicates(["one", "one", "two"]) // true
+ * hasDuplicates(["one", "two", "three"]) // false
+ * ```
  *
  * @public
  */
