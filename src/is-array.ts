b23165733f8d89464ad0286b5a4cc0461b6d25b4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-29 21:20:28 +0330

    docs: update comments

--- src/is-array.ts
+++ src/is-array.ts
@@ -1,3 +1,9 @@
+/**
+ * Determines whether the input is an array or not.
+ * @param value - Generic input.
+ * @returns array
+ *
+ */
 export function isArray<T>(value: any): value is Array<T> {
   return Array.isArray(value);
 }
