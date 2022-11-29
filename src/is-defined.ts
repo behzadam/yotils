fd86b7a52da38ddb532fff339084aaaec8212741
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 02:26:28 +0330

    docs: add comments

--- src/is-defined.ts
+++ src/is-defined.ts
@@ -1,3 +1,20 @@
+/**
+ * Determines whether the input is defined or not.
+ * @param value - input
+ * @returns true if the input is defined, false otherwise.
+ * @remarks
+ * This function returns NonNullable<T> to avoid null check after the revoke.
+ *
+ * @example
+ * ```ts
+ * declare let val: string | undefined;
+ * if (isDefined(val)) {
+ *   // val is defined and safe to use.
+ *   val.toLocaleLowerCase()
+ * }
+ * ```
+ * @public
+ */
 export function isDefined<T>(value: T): value is NonNullable<T> {
   return value !== undefined && value !== null;
 }
