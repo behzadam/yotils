d36cac6c5ea12462e2c81bacf1f6786db32abc9a
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 00:40:31 +0330

    docs: update comments

--- src/is-date.ts
+++ src/is-date.ts
@@ -1,3 +1,15 @@
+/**
+ * Determines whether the input is a Date type or not.
+ * @param value - input.
+ * @returns true if the input is a Date type, false otherwise.
+ * @example
+ * ```ts
+ * isDate(new Date(2000, 1, 1)) // true
+ * isDate('29 Feb 2020') // false
+ * ```
+ *
+ * @public
+ */
 export function isDate(value: any): value is Date {
   return value instanceof Date;
 }
