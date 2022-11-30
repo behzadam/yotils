db6d2352abe6a77b8b88c47f4655a7e7bcc0723f
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 23:59:44 +0330

    fix: add & object to return function

--- src/is-object.ts
+++ src/is-object.ts
@@ -1,4 +1,4 @@
-export function isObject<T>(value: T): value is T {
+export function isObject<T>(value: T): value is T & object {
   const type = typeof value;
   return value != null && (type === "object" || type === "function");
 }
