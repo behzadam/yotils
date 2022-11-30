9ae6a83ed87902036710820b4e1212ed21985175
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 23:55:20 +0330

    test: merge test cases

--- __tests__/is-empty.test.ts
+++ __tests__/is-empty.test.ts
@@ -1,18 +1,15 @@
 import { isEmpty } from "@lib";
-import { describeGen } from "./utils/describe-gen";
 
-describe(describeGen(isEmpty.name).valid, () => {
-  test("should return true on empty values", () => {
-    expect(isEmpty({})).toBe(true);
-    expect(isEmpty([])).toBe(true);
-    expect(isEmpty("")).toBe(true);
-  });
-});
-
-describe(describeGen(isEmpty.name).invalid, () => {
-  test("should return false on non empty values", () => {
-    expect(isEmpty({ id: 1 })).toBe(false);
-    expect(isEmpty([1, 2])).toBe(false);
-    expect(isEmpty("hello")).toBe(false);
+describe("isEmpty cases", () => {
+  test.each`
+    input        | expected
+    ${[]}        | ${true}
+    ${{}}        | ${true}
+    ${""}        | ${true}
+    ${{ id: 1 }} | ${false}
+    ${[1]}       | ${false}
+    ${"1"}       | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isEmpty(input)).toBe(expected);
   });
 });
