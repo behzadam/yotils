299be3c32e0ddf301b78a285fb37b9283ad18da6
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-30 23:38:00 +0330

    test: add test cases

---
+++ __tests__/is-object.test.ts
@@ -0,0 +1,14 @@
+import { isObject } from "@lib";
+
+describe("isObject cases", () => {
+  test.each`
+    input        | expected
+    ${{}}        | ${true}
+    ${{ id: 1 }} | ${true}
+    ${Function}  | ${true}
+    ${[1, 2, 3]} | ${true}
+    ${null}      | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isObject(input)).toBe(expected);
+  });
+});
