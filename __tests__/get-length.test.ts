b1b4efff429675bd15351fce7f2e12193725e776
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-02 13:09:15 +0330

    test: add test cases

---
+++ __tests__/get-length.test.ts
@@ -0,0 +1,15 @@
+import { getLength } from "@lib";
+
+describe("getLength cases", () => {
+  test.each`
+    input        | expected
+    ${[]}        | ${0}
+    ${[1, 2, 3]} | ${3}
+    ${{}}        | ${0}
+    ${{ id: 1 }} | ${1}
+    ${true}      | ${0}
+    ${1}         | ${0}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(getLength(input)).toBe(expected);
+  });
+});
