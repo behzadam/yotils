9d0291b6b831e73a4ab8987e330bea03d124485c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 10:35:40 +0330

    docs: add comment

--- src/is-empty.ts
+++ src/is-empty.ts
@@ -1,5 +1,23 @@
 import { getLength } from "./get-length";
 
+/**
+ * Determines whether an input is empty or not.
+ * @param value - input.
+ * @returns true if the input is empty, false otherwise.
+ * @example
+ * ```ts
+ *  []       | ${true}
+ *  {}       | ${true}
+ *  ""       | ${true}
+ *  null     | ${true}
+ *  true     | ${true}
+ *  1        | ${true}
+ *  id: 1    | ${false}
+ *  [1]      | ${false}
+ *  "1"      | ${false}
+ * ```
+ * @public
+ */
 export function isEmpty<T>(value: T): value is T {
   if (value == null) return true;
   return getLength(value) === 0;
