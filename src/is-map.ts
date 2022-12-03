b5dd62d522dc48bc04ebf59eb988224c5fcc1947
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 13:10:52 +0330

    docs: add comments

--- src/is-map.ts
+++ src/is-map.ts
@@ -1,3 +1,15 @@
+/**
+ * Determines whether an input is a map or not.
+ * @param value - input
+ * @returns true if the input is a map, false otherwise.
+ * @example
+ * ```ts
+ *  isMap(new Map<string, number>()) // true
+ *  isMap({ id: 1 })                 // false
+ *  isMap([1, 2, 3])                 // false
+ * ```
+ * @public
+ */
 export function isMap<T>(value: T): value is T {
   return value instanceof Map;
 }
