bf1aa6617efc3009f17f9bec43b76c948de93629
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 13:03:25 +0330

    test: add test cases

---
+++ __tests__/is-map.test.ts
@@ -0,0 +1,12 @@
+import { isMap } from "@lib";
+
+describe("isObject cases", () => {
+  test.each`
+    input                        | expected
+    ${new Map<string, number>()} | ${true}
+    ${{ id: 1 }}                 | ${false}
+    ${[1, 2, 3]}                 | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isMap(input)).toBe(expected);
+  });
+});
