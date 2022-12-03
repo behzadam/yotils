1c70def620ec1a133c527eac70505a1ce69ea53a
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 13:53:54 +0330

    test: add isNumber test cases

---
+++ __tests__/is-number.test.ts
@@ -0,0 +1,13 @@
+import { isNumber } from "@lib";
+
+describe("isNumber cases", () => {
+  test.each`
+    input    | expected
+    ${1}     | ${true}
+    ${{}}    | ${false}
+    ${[]}    | ${false}
+    ${false} | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isNumber(input)).toBe(expected);
+  });
+});
