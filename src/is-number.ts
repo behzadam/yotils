3f8a572dd3f876d808086654c12bdf188bd11856
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 13:56:29 +0330

    docs: add comments

--- src/is-number.ts
+++ src/is-number.ts
@@ -1,3 +1,16 @@
+/**
+ * Determines whether an input is number or not.
+ * @param value - input
+ * @returns true if the input is number, false otherwise.
+ * @example
+ * ```ts
+ * isNumber(1)     // true
+ * isNumber({})    // false
+ * isNumber([])    // false
+ * isNumber(true)  // false
+ * ```
+ * @public
+ */
 export function isNumber(value: unknown): value is number {
   return typeof value === "number";
 }
