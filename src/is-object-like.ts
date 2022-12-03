fef32226cae9672cef3bd1c4fe88ada6794d04f5
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-03 15:35:59 +0330

    docs: add comments

--- src/is-object-like.ts
+++ src/is-object-like.ts
@@ -1,3 +1,17 @@
+/**
+ * Determines whether the input is an object like or not.
+ * When a value is object like, it is not null and `typeof` result is "object".
+ * @param value - input.
+ * @returns true when the input is an object like, or false when it is not.
+ * @example
+ * ```ts
+ * isObjectLike({})         // true
+ * isObjectLike([])         // true
+ * isObjectLike(Function)   // false
+ * isObjectLike(null)       // false
+ * ```
+ * @public
+ */
 export function isObjectLike<T>(value: T): value is T & object {
   return typeof value === "object" && value !== null;
 }
