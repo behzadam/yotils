8c4d3ef59b415aa0f11f0511db374a79dbe3714e
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 23:29:26 +0330

    docs: add comments

--- src/internal/_get-tag.ts
+++ src/internal/_get-tag.ts
@@ -1,3 +1,21 @@
+/**
+ * Calls `toString` with `value` and returns a string object type like "[object Object]".
+ * @param value - input
+ * @returns string type like "[object Object]".
+ * @example
+ * ```ts
+ *  getTag(new Set())   // "[object Set]"
+ *  getTag(new Map())   // "[object Map]"
+ *  getTag({})          // "[object Object]"
+ *  getTag("")          // "[object String]"
+ *  getTag(1)           // "[object Number]"
+ *  getTag(null)        // "[object Null]"
+ *  getTag(undefined)   // "[object Undefined]"
+ *  getTag(new Date())  // "[object Date]"
+ *  getTag(Symbol())    // "[object Symbol]"
+ * ```
+ * @internal
+ */
 export function getTag<T>(value: T): string {
   return Object.prototype.toString.call(value);
 }
