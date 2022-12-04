873b4cf036d3caf0a624968c72f177c1e27c3edc
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 16:21:28 +0330

    docs: add comments

--- src/is-null.ts
+++ src/is-null.ts
@@ -1,3 +1,15 @@
+/**
+ * Determines whether an input is null or not.
+ * @param value - input
+ * @returns true if the input is null, false otherwise.
+ * @example
+ * ```ts
+ * isNull(null)      // true
+ * isNull(undefined) // false
+ * isNull({})        // false
+ * ```
+ * @public
+ */
 export function isNull(value: unknown): value is null {
   return value === null;
 }
