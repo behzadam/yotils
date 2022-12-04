8640c07f4b720612766dad96de85ea7cdca17114
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 13:45:04 +0330

    test: add some test cases

--- src/internal/get-tag.test.ts
+++ src/internal/get-tag.test.ts
@@ -4,6 +4,10 @@ describe("getTag cases", () => {
   test.each`
     input        | expected
     ${new Set()} | ${"[object Set]"}
+    ${new Map()} | ${"[object Map]"}
+    ${{}}        | ${"[object Object]"}
+    ${""}        | ${"[object String]"}
+    ${1}         | ${"[object Number]"}
   `("should return $expected when input is: $input", ({ input, expected }) => {
     expect(getTag(input)).toBe(expected);
   });
