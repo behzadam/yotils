b8e24f854ee63aaf31018cbf7fe65c922fc0f952
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-05 01:55:42 +0330

    test: add Infinity | NaN | "1" to test cases

--- src/is-number.test.ts
+++ src/is-number.test.ts
@@ -2,11 +2,11 @@ import { isNumber } from "./";
 
 describe("isNumber cases", () => {
   test.each`
-    input    | expected
-    ${1}     | ${true}
-    ${{}}    | ${false}
-    ${[]}    | ${false}
-    ${false} | ${false}
+    input       | expected
+    ${1}        | ${true}
+    ${NaN}      | ${false}
+    ${Infinity} | ${false}
+    ${"1"}      | ${false}
   `("should return $expected when input is: $input", ({ input, expected }) => {
     expect(isNumber(input)).toBe(expected);
   });
