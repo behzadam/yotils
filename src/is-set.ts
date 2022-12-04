ee89256a3229662ec254ec9a02d9381917106fe9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 23:52:41 +0330

    docs: add comments

--- src/is-set.ts
+++ src/is-set.ts
@@ -1,6 +1,16 @@
 import { getTag } from "./internal";
 import { isObjectLike } from "./is-object-like";
 
+/**
+ * Determines whether the input is `Set` or not.
+ * @param value - input.
+ * @returns true if the input is `Set`, false otherwise.
+ * @example
+ * ```ts
+ * isSet(new Set()) // true
+ * isSet([])        // false
+ * ```
+ */
 export function isSet<T>(value: T): value is T {
   return isObjectLike(value) && getTag(value) === "[object Set]";
 }
