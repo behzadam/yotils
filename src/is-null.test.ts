d74fd3f8f973481bb7c6d5442f2ddd71d59654c9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 16:20:37 +0330

    test: add isNull test cases

---
+++ src/is-null.test.ts
@@ -0,0 +1,13 @@
+import { isNull } from "./";
+
+describe("isNull cases", () => {
+  test.each`
+    input        | expected
+    ${null}      | ${true}
+    ${undefined} | ${false}
+    ${{}}        | ${false}
+    ${[]}        | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isNull(input)).toBe(expected);
+  });
+});
