edcc1de8052083e0e7647fff9272299ab081f233
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 23:25:08 +0330

    test: add some test cases

--- src/internal/get-tag.test.ts
+++ src/internal/get-tag.test.ts
@@ -2,12 +2,16 @@ import { getTag } from "./index";
 
 describe("getTag cases", () => {
   test.each`
-    input        | expected
-    ${new Set()} | ${"[object Set]"}
-    ${new Map()} | ${"[object Map]"}
-    ${{}}        | ${"[object Object]"}
-    ${""}        | ${"[object String]"}
-    ${1}         | ${"[object Number]"}
+    input         | expected
+    ${new Set()}  | ${"[object Set]"}
+    ${new Map()}  | ${"[object Map]"}
+    ${{}}         | ${"[object Object]"}
+    ${""}         | ${"[object String]"}
+    ${1}          | ${"[object Number]"}
+    ${null}       | ${"[object Null]"}
+    ${undefined}  | ${"[object Undefined]"}
+    ${new Date()} | ${"[object Date]"}
+    ${Symbol()}   | ${"[object Symbol]"}
   `("should return $expected when input is: $input", ({ input, expected }) => {
     expect(getTag(input)).toBe(expected);
   });
