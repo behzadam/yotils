bde5a838744a1491719336d18d1126061c394c95
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-04 23:40:19 +0330

    docs: add link to comments

--- src/internal/_get-tag.ts
+++ src/internal/_get-tag.ts
@@ -1,7 +1,10 @@
 /**
  * Calls `toString` with `value` and returns a string object type like "[object Object]".
  * @param value - input
- * @returns string type like "[object Object]".
+ * @returns string type like "[object Object]":
+ * @remarks
+ * A deeper explanation of [object Object]:
+ * {@link https://betterprogramming.pub/what-is-object-object-in-javascript-object-prototype-tostring-1db888c695a4 | Link}
  * @example
  * ```ts
  *  getTag(new Set())   // "[object Set]"
