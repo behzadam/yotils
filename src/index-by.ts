fa8060093d1d94161628ddfd71e47c6d448205c0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 01:41:49 +0330

    docs: add comments

--- src/index-by.ts
+++ src/index-by.ts
@@ -1,3 +1,35 @@
+/**
+ * Returns the spilted lists grouped by index.
+ * They key should be unique and for  ununique keys it would be replaced with the last key.
+ * @param array - of objects.
+ * @param key - unique key
+ * @returns grouped by index, key value list.
+ * @example
+ * ```ts
+ * indexBy([
+ *  { id: 1, name: "A" },
+ *  { id: 2, name: "B" },
+ *  { id: 3, name: "C" },
+ *  ], 'id')
+ *  // result:
+ *  // {
+ *  //  "1": { id: 1, name: "A" },
+ *  //  "2": { id: 2, name: "B" },
+ *  //  "3": { id: 3, name: "C" },
+ *  // }
+ *  indexBy([
+ *  { id: 1, name: "A" },
+ *  { id: 1, name: "B" },
+ *  { id: 3, name: "C" },
+ *  ], 'id')
+ *  // result:
+ *  // {
+ *  //  "1": { id: 1, name: "B" },
+ *  //  "3": { id: 3, name: "C" },
+ *  // }
+ * ```
+ * @public
+ */
 export function indexBy<
   T extends { [K in keyof T]: string | number | symbol },
   K extends keyof T
