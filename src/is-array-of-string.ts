a8310de04218e3030475f2afea9da14bfc45a19f
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-05 14:30:14 +0330

    docs: add comments

--- src/is-array-of-string.ts
+++ src/is-array-of-string.ts
@@ -1,5 +1,16 @@
 import { isString } from "./is-string";
 
+/**
+ * Determines whether the input is array of string or not.
+ * @param value - input
+ * @returns true if the input is array, false otherwise.
+ * @example
+ * ```ts
+ * isArrayOfString(["a", "b", "c"]) // true
+ * isArrayOfString(["a", "b", 1])   // false
+ * ```
+ * @public
+ */
 export function isArrayOfString(value: unknown): value is string[] {
   return Array.isArray(value) && value.every((item) => isString(item));
 }
