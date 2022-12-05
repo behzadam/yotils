4f0fb9c6f4a2f95e2cbbac06a3791f4112ef30b3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 00:31:02 +0330

    test: add duplicated keys test cases

--- src/index-by.test.ts
+++ src/index-by.test.ts
@@ -1,21 +1,34 @@
 import { indexBy } from ".";
 
 describe("indexBy cases", () => {
-  const input = [
+  const withUniqueKey = [
     { id: 1, name: "A" },
     { id: 2, name: "B" },
     { id: 3, name: "C" },
   ];
 
-  const expected = {
+  const withUniqueKeyExpected = {
     "1": { id: 1, name: "A" },
     "2": { id: 2, name: "B" },
     "3": { id: 3, name: "C" },
   };
 
+  const withDuplicatedKey = [
+    { id: 1, name: "A" },
+    { id: 1, name: "B" },
+    { id: 3, name: "C" },
+  ];
+
+  // On duplicate keys should be replaced the repeated key with the last one.
+  const withDuplicatedKeyExpected = {
+    "1": { id: 1, name: "B" },
+    "3": { id: 3, name: "C" },
+  };
+
   test.each`
-    input    | expected
-    ${input} | ${expected}
+    input                | expected
+    ${withUniqueKey}     | ${withUniqueKeyExpected}
+    ${withDuplicatedKey} | ${withDuplicatedKeyExpected}
   `("should return $expected when input is: $input", ({ input, expected }) => {
     expect(indexBy([...input], "id")).toStrictEqual(expected);
   });
