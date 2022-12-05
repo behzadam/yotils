2dcad9d660a79218a45fe7438ea83aa039f225bf
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-05 13:46:28 +0330

    docs: add comments

--- src/is-string.ts
+++ src/is-string.ts
@@ -1,3 +1,16 @@
+/**
+ * Determines whether the input is string or not.
+ * @param value - input
+ * @returns true if the input is string, false otherwise.
+ * @example
+ * ```ts
+ * isString("string") // true
+ * isString('string') // true
+ * isString(`string`) // true
+ * isString(123)      // false
+ * ```
+ * @public
+ */
 export function isString(value: unknown): value is string {
   return typeof value === "string";
 }
