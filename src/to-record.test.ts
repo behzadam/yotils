77310e8058c757b56db3410fd42cc912d00cb372
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 00:07:30 +0330

    test: update test cases

--- src/to-record.test.ts
+++ src/to-record.test.ts
@@ -1,18 +1,22 @@
 import { toRecord } from "./";
-import { describeGen } from "./utils/describe-gen";
 
-describe(describeGen(toRecord.name).valid, () => {
-  test("should return record of an array", () => {
-    const array = [
-      { id: 1, name: "A" },
-      { id: 2, name: "B" },
-      { id: 3, name: "C" },
-    ];
-    const record = toRecord(array, "id");
-    expect(record).toStrictEqual({
-      "1": { id: 1, name: "A" },
-      "2": { id: 2, name: "B" },
-      "3": { id: 3, name: "C" },
-    });
+describe("toRecord cases", () => {
+  const input = [
+    { id: 1, name: "A" },
+    { id: 2, name: "B" },
+    { id: 3, name: "C" },
+  ];
+
+  const expected = {
+    "1": { id: 1, name: "A" },
+    "2": { id: 2, name: "B" },
+    "3": { id: 3, name: "C" },
+  };
+
+  test.each`
+    input    | expected
+    ${input} | ${expected}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(toRecord([...input], "id")).toStrictEqual(expected);
   });
 });
