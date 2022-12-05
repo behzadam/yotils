40e213a82cb91c04b4d0e74f45071baf1e972dfd
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-05 14:24:38 +0330

    test: add isArrayOfString test cases

---
+++ src/is-array-of-string.test.ts
@@ -0,0 +1,11 @@
+import { isArrayOfString } from "./";
+
+describe("isArrayOfString cases", () => {
+  test.each`
+    input              | expected
+    ${["a", "b", "c"]} | ${true}
+    ${["a", "b", 1]}   | ${false}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(isArrayOfString(input)).toBe(expected);
+  });
+});
