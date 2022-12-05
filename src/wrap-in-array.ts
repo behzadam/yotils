75fbb4e32e950bdd9594b9b6990d03a3114c8c25
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-05 15:28:22 +0330

    docs: add comments

--- src/wrap-in-array.ts
+++ src/wrap-in-array.ts
@@ -1,3 +1,15 @@
+/**
+ * Wraps the input in an array even if the input is an array.
+ * @param value - input
+ * @returns wrapped input in an array.
+ * @example
+ * ```ts
+ * wrapInArray('a')     // ['a']
+ * wrapInArray(1)       // [1]
+ * wrapInArray(['a'])   // [['a']]
+ * ```
+ * @public
+ */
 export function wrapInArray<Type>(value: Type): Type[] {
   return [value];
 }
