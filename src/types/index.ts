71170f78483869f63cc68392517b0b2ab4eafc41
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 20:30:36 +0330

    feat: add PredicateFunction

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 export type Primitive = string | number | boolean;
 
 export type Dictionary<T, K extends string | number | symbol = string> = {
-  [key in K]: T;
+  [key in K]?: T;
 };
+
+export type PredicateFunction = (...args: any[]) => boolean;
