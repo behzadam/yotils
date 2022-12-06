689151bc7427326fa403706f37d840a2995d4c54
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 22:42:20 +0330

    feat: add AnyObject type

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,4 +4,11 @@ export type Dictionary<T, K extends string | number | symbol = string> = {
   [key in K]?: T;
 };
 
+export type AnyObject = Record<keyof any, unknown>;
+
+// export type PredicateFunction<T> = <K extends keyof T>(
+//   key: K,
+//   value: T[K]
+// ) => boolean;
+
 export type PredicateFunction = (...args: any[]) => boolean;
