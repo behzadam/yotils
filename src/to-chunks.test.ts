bb2ea3809830939cd1ff6056ee3f7d03f2ece228
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-06 13:41:40 +0330

    test: update test cases

--- src/to-chunks.test.ts
+++ src/to-chunks.test.ts
@@ -1,11 +1,11 @@
 import { toChunks } from "./";
 
-describe("splitIntoChunks", () => {
-  test("should return array of chunks or an empty array", () => {
-    const chunks = toChunks(
-      "Lorem ipsum dolor sit amet, consectetur adipis",
-      4
-    );
-    expect(chunks.length).toBe(4);
+describe("toChunks cases", () => {
+  test.each`
+    input                                  | expected
+    ${"Lorem ipsum is a placeholder text"} | ${["Lorem ips", "um is a p", "laceholde", "r text"]}
+    ${"Lorem"}                             | ${["Lo", "re", "m"]}
+  `("should return $expected when input is: $input", ({ input, expected }) => {
+    expect(toChunks(input, 4)).toStrictEqual(expected);
   });
 });
