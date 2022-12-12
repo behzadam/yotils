bd7f975249ea1b7250b9d29eca754ac559615ae6
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-12 23:49:44 +0330

    test: remove describeGen

--- src/is-equal-date.test.ts
+++ src/is-equal-date.test.ts
@@ -1,19 +1,9 @@
 import { isEqualDate } from "./";
-import { describeGen } from "./utils/describe-gen";
 
-describe(describeGen(isEqualDate.name).valid, () => {
+describe("isEqualDate cases", () => {
   it("should return true when two dates are equal", () => {
     const target = new Date(2016, 1, 1);
     const other = new Date(2016, 1, 1);
-
-    expect(isEqualDate(target, other)).toBe(true);
-  });
-});
-
-describe(describeGen(isEqualDate.name).invalid, () => {
-  it("should return false when two dates are not equal", () => {
-    const target = new Date(2016, 1, 1);
-    const other = new Date(2017, 1, 1);
 
     expect(isEqualDate(target, other)).toBe(true);
   });
