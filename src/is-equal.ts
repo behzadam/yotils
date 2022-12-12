89a1967f959051475d1b60984614596652fa004b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-12 23:31:15 +0330

    refactor: add equals for date

--- src/is-equal.ts
+++ src/is-equal.ts
@@ -1,3 +1,28 @@
+import { isDate } from "./is-date";
+import { isEqualDate } from "./is-equal-date";
+import { isObjectLike } from "./is-object-like";
+
+/**
+ * Determines whether two inputs are equal or not.
+ * @param target - The target.
+ * @param other - The other.
+ * @returns true if equals.
+ * @alpha
+ */
 export function isEqual<T>(target: T, other: T): boolean {
-  return target === other;
+  if (target === other) return true;
+  if (isDate(target) && isDate(other)) {
+    return isEqualDate(target, other);
+  }
+
+  if (
+    target === null ||
+    other === null ||
+    (!isObjectLike(target) && !isObjectLike(other))
+  ) {
+    // self qualities
+    return target !== target && other !== other;
+  }
+  // TODO: should complete with recursive.
+  return false;
 }
