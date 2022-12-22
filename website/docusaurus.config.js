d409202e719c7a8dec213b960c86ca483276f2a5
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-12-22 22:19:26 +0330

    chore: update configs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
@@ -6,22 +7,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Yotils',
+  tagline: 'Typescript utility belt',
+  url: 'https://behzadam.github.io/',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  organizationName: 'behzadam',
+  projectName: 'yotils', // Usually your repo name.
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -48,18 +42,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Yotils',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Yotils Logo',
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Tutorial',
-          },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
