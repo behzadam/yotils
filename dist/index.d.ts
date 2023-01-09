149d1b734b7e041abdfe629a992652eefc6e145b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-01-09 19:01:57 +0330

    chore: generate types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,21 +6,6 @@
 
 declare type AnyObject = Record<keyof any, unknown>;
 
-/**
- * Compares all items in the given array and returns true if they are same.
- * @param array - Given array.
- * @returns true | false
- * @example
- *
- * ```ts
- * areSame([1, 1, 1, 1]) // true
- * areSame(["one", "one", "one"]) // true
- * areSame([1, 2]) // false
- * ```
- * @public
- */
-export declare function areSame<T extends string | number | boolean>(array: Required<T[]>): boolean;
-
 /* Excluded from this release type: countItems */
 
 /**
--- dist/yotils.d.ts
+++ dist/yotils.d.ts
@@ -7,21 +7,6 @@
 declare type AnyObject = Record<keyof any, unknown>;
 
 /**
- * Compares all items in the given array and returns true if they are same.
- * @param array - Given array.
- * @returns true | false
- * @example
- *
- * ```ts
- * areSame([1, 1, 1, 1]) // true
- * areSame(["one", "one", "one"]) // true
- * areSame([1, 2]) // false
- * ```
- * @public
- */
-export declare function areSame<T extends string | number | boolean>(array: Required<T[]>): boolean;
-
-/**
  * Counts of repeated items in flattened arrays.
  * @param array - array of items.
  * @returns - key value list
