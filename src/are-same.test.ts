fa87372b2587daa502687fb663757950cbb15a5a
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-06-26 11:55:35 +0330

    test(refactor): merge test cases

--- src/are-same.test.ts
+++ src/are-same.test.ts
@@ -1,20 +1,12 @@
-import { areSame } from './';
+import { areSame } from './are-same';
 
-describe('areSame valid cases', () => {
-  test.each`
-    input                    | expected
-    ${[1, 1, 1]}             | ${true}
-    ${['one', 'one', 'one']} | ${true}
-    ${[1.5, 1.5, 1.5]}       | ${true}
-    ${[true, true, true]}    | ${true}
-  `('should return $expected when input is: $input', ({ input, expected }) => {
-    expect(areSame(input)).toBe(expected);
-  });
-});
-
-describe('areEqual invalid cases', () => {
+describe('areSame', () => {
   test.each`
     input                      | expected
+    ${[1, 1, 1]}               | ${true}
+    ${['one', 'one', 'one']}   | ${true}
+    ${[1.5, 1.5, 1.5]}         | ${true}
+    ${[true, true, true]}      | ${true}
     ${[1, 2, 3]}               | ${false}
     ${['one', 'two', 'three']} | ${false}
     ${[1.5, 2.5, 3.5]}         | ${false}
